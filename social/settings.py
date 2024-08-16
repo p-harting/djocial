c1bece9e755deb5c75e4525fc7a31d98e8ded2ec
Pascal Harting <pascalharting98@gmail.com>
2024-08-16 22:23:01 +0000

    Prepare code for deployment

--- social/settings.py
+++ social/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%2o%8+bj8i8mn84y906yt0hy9ak9$bh&v6w4n$uh0&$g*i^9r('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost',
                  '.herokuapp.com',]
