4eded5de08369c3e6afe783525b8a80ff34e1854
Pascal Harting <pascalharting98@gmail.com>
2024-08-17 16:00:29 +0000

    Show latest posts on feed / index page

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,10 @@
-from django.views import generic
+from django.views.generic import TemplateView
+from .models import Post
 
-class HomeView(generic.TemplateView):
+class HomeView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.filter(status=1).order_by('-created_on')
+        return context
