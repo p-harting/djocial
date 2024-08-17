cd9175091a2ae289b41f5e90ac3ff77af2793843
Pascal Harting <pascalharting98@gmail.com>
2024-08-17 12:58:18 +0000

    Change SECRET_KEY

--- social/settings.py
+++ social/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%2o%8+bj8i8mn84y906yt0hy9ak9$bh&v6w4n$uh0&$g*i^9r('
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
                  '.herokuapp.com',]
