da82974eb2eb330624cc4d1ec159e533163774c5
Pascal Harting <pascalharting98@gmail.com>
2024-08-17 22:02:51 +0000

    Add editable account pages

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,11 @@
 from . import views
 from django.urls import path
-from .views import HomeView, PostDetailView, CreatePostView, LikeView
+from .views import HomeView, PostDetailView, CreatePostView, LikeView, AccountView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('post/<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
     path('create/', CreatePostView.as_view(), name='create_post'),
     path('like/<slug:slug>/', LikeView, name='like_post'),
+    path('account/<int:pk>', AccountView.as_view(), name='account'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -9,6 +9,15 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django import forms
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import PasswordChangeForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.views.generic import DetailView
 
 class HomeView(TemplateView):
     template_name = 'index.html'
@@ -49,3 +58,51 @@ def LikeView(request, slug):
     else:
         post.likes.add(request.user)
     return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+class AccountView(LoginRequiredMixin, DetailView):
+    model = User
+    template_name = 'account.html'
+    context_object_name = 'user_account'
+
+    def get_object(self, queryset=None):
+        return User.objects.get(pk=self.kwargs['pk'])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_own_account'] = self.request.user == self.get_object()
+        
+        if context['is_own_account']:
+            context['email_form'] = forms.Form()  # Placeholder for the email form
+            context['password_form'] = PasswordChangeForm(user=self.request.user)
+        
+        return context
+
+    def post(self, request, *args, **kwargs):
+        user = self.get_object()
+        if request.user != user:
+            return redirect('home')  # Prevent users from updating others' accounts
+
+        if 'email_form' in request.POST:
+            email = request.POST.get('email')
+            if email and not User.objects.filter(email=email).exists():
+                user.email = email
+                user.save()
+                messages.success(request, 'Your email has been updated successfully!')
+                return redirect('account', pk=user.pk)
+            else:
+                messages.error(request, 'This email is already in use or invalid.')
+        
+        elif 'password_form' in request.POST:
+            password_form = PasswordChangeForm(user=request.user, data=request.POST)
+            if password_form.is_valid():
+                user = password_form.save()
+                update_session_auth_hash(request, user)  # Important to keep the user logged in after password change
+                messages.success(request, 'Your password has been updated successfully!')
+                return redirect('account', pk=user.pk)
+            else:
+                messages.error(request, 'Please correct the errors below.')
+        
+        context = self.get_context_data()
+        context['email_form'] = forms.Form()  # Placeholder for the email form
+        context['password_form'] = PasswordChangeForm(user=request.user, data=request.POST)
+        return render(request, self.template_name, context)
