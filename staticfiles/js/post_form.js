8911341a71a99efe47cfea1109d9bccf8acbe5c5
Pascal Harting <pascalharting98@gmail.com>
2024-08-23 21:59:34 +0000

    Collect static files

--- staticfiles/js/post_form.js
+++ staticfiles/js/post_form.js
@@ -2,8 +2,12 @@ document.addEventListener("DOMContentLoaded", function() {
     const textarea = document.getElementById("postContent");
     const charCount = document.getElementById("charCount");
 
-    textarea.addEventListener("input", function() {
-        const currentLength = textarea.value.length;
-        charCount.textContent = `${currentLength}/350 characters`;
-    });
+    if (textarea && charCount) {
+        textarea.addEventListener("input", function() {
+            const currentLength = textarea.value.length;
+            charCount.textContent = `${currentLength}/350 characters`;
+        });
+    } else {
+        console.error("Element(s) not found: ", { textarea, charCount });
+    }
 });
